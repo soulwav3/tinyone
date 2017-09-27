3ded8c692012f970f5c6bd1ce45a2a7a08fa560b
Anton Petrov <anttonpettrov@gmail.com>
2017-09-27 19:29:48 +0300

    Completed footer section

--- gulp/styles.js
+++ gulp/styles.js
@@ -1,16 +1,17 @@
 const gulp = require('gulp'),
 postcss = require('gulp-postcss'),
 nested = require('postcss-nested'),
-mixins = require('postcss-mixins');
+mixins = require('postcss-mixins'),
 cssImport = require('postcss-import'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars'),
 hexrgba = require('postcss-hexrgba'),
+colorFunction = require('postcss-color-function'),
 rename = require('gulp-rename');
 
 gulp.task('styles', () => {
   return gulp.src('./src/styles/style.pcss')
-          .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
+          .pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, colorFunction, autoprefixer]))
           .on('error', function(errorInfo) {
             console.log(errorInfo.toString());
             this.emit('end');
